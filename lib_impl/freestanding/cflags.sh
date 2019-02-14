030a300f4cb3ca7a7465af42073ebeb864db11cc
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2019-02-14 17:20:13 +0100

    Get rid of pkg-config + new package definitions
    For mirage/mirage#969
    Along with:
    mirage-platform: 0612bdbfe1eeb004ad923991178256feb780d14c
    ocaml-freestanding: c5bf86ce29872b65a12ac42b9104f15f063d644e
    
    Instead of using pkg-config, cflags and libs are fetched from files in
    the library folder.

--- lib_impl/freestanding/cflags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export PKG_CONFIG_PATH="$(opam config var lib)/pkgconfig"
-flags="$(pkg-config --static mirage-xen-posix --cflags)"
-if [ "$(uname -m)" = "x86_64" -o "$(uname -m)" = "x86" ]
-then
-    flags="$flags -mrdrnd -mrdseed"
-fi
-echo "($flags)"
